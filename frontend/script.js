395e1482080c771e7e49faf962d89461752965a5
Sourav <roysourav1812@gmail.com>
2026-04-18 19:07:23 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -137,7 +137,7 @@ async function loadTimer() {
   } catch (error) {
     document.getElementById("event-name").innerText = "আশার আলো, ছকে খেলো";
     document.getElementById("organizer-name").innerText = "Dipan Bandyopadhyay & Team™";
-    runCountdown(new Date("2026-04-18T22:00:00").getTime());
+    runCountdown(new Date("2026-04-18T19:25:30").getTime());
   }
 }
 
